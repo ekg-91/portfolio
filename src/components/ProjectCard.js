c8288ee4c6f009ec03e914633042f3a70400fa26
Elliot Gallagher <elliot@crates.media>
2020-04-22 16:26:45 -0400

    pre-refactor initial commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 
 function ProjectCard (props) {
   return (
-    <Card>
+    <Card className='p-card'>
       <Card.Img variant='top' src={props.item.imgSrc} />
       <Card.Body>
         <Card.Title>{props.item.title}</Card.Title>
--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import CardDeck from 'react-bootstrap/CardDeck';
-
 import ProjectCard from './ProjectCard';
+
+import ReactLogo from '../assets/images/react-logo.png';
 
 class ProjectsGrid extends React.Component {
   constructor (props) {
@@ -15,25 +15,25 @@ class ProjectsGrid extends React.Component {
           id: 0,
           title: 'Project 1',
           description: 'This is sample text',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
-          demoLink: '',
+          demoLink: '/',
           selected: false
         },
         {
           id: 1,
           title: 'Project 2',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
-          demoLink: '',
+          demoLink: '/',
           selected: false
         },
         {
           id: 2,
           title: 'Project 3',
           description: 'This will say something about this project',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           selected: false
         },
@@ -41,7 +41,7 @@ class ProjectsGrid extends React.Component {
           id: 3,
           title: 'Project 4',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
@@ -50,7 +50,7 @@ class ProjectsGrid extends React.Component {
           id: 4,
           title: 'Project 5',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
@@ -59,7 +59,7 @@ class ProjectsGrid extends React.Component {
           id: 5,
           title: 'Project 6',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
@@ -68,7 +68,7 @@ class ProjectsGrid extends React.Component {
           id: 6,
           title: 'Project 7',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
@@ -77,7 +77,7 @@ class ProjectsGrid extends React.Component {
           id: 7,
           title: 'Project 8',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
@@ -86,45 +86,49 @@ class ProjectsGrid extends React.Component {
           id: 8,
           title: 'Project 9',
           description: 'This is a fake description',
-          imgSrc: '',
+          imgSrc: ReactLogo,
           gitLink: '',
           demoLink: '',
           selected: false
-        },
+        }
       ]
     };
   }
 
-  makeItems = (items) => { 
-    let chunk = () => {
-      let chunked = [];
-      while(items.length) {
-        chunked.push(items.splice(0, 3))
-      }
-      return chunked
-    };
+  chunk (items) {
+    const chunked = [];
 
-    let gridItems = chunk();
-
-    for(let i = 0; i < gridItems.length; i++) {
-      let row = [...gridItems[i]];
-      console.log(row);
-      
-      // return row.map(item => {<ProjectCard item={item} title={item.title} />});
+    while (items.length) {
+      chunked.push(items.splice(0, 3));
     }
-    
-    // return items.map(item => {
-    //   return <ProjectCard item={item} title={item.title} />
-    // });
+
+    return chunked;
+  }
+
+  makeItems (items) {
+    const gridItems = this.chunk(items);
+    const rows = [];
+
+    for (let i = 0; i < gridItems.length; i++) {
+      rows.push([...gridItems[i]]);
+    }
+
+    let incr = 0;
+    const mappedRows = rows.map(row => {
+      incr++;
+      return <Row key={incr}>
+        {row.map(item => <ProjectCard item={item} title={item.title} key={item.id} />)}
+      </Row>;
+    });
+
+    return mappedRows;
   }
 
   render () {
     return (
       <Container>
         <CardDeck>
-          <Row>
-            {this.makeItems(this.state.items)}
-          </Row>
+          {this.makeItems(this.state.items)}
         </CardDeck>
       </Container>
     );
